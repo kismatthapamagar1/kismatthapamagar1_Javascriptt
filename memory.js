d63c2030a9b502173cba74854a96b663151ca399
kismatthapamagar1 <000kismatthapa@gmail.com>
2026-08-28 13:39:46 +0545

    heap and stack memory

---
+++ memory.js
@@ -0,0 +1,7 @@
+// Stack(primitive) and heap memory(non primitive)
+
+let myLaptopName="lenovo"
+let anotherName =myLaptopName
+anothername="Dell"
+console.log(myLaptopName)
+console.log(anotherName)
