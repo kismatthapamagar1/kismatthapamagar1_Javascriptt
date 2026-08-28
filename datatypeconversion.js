b12fa3031bb5fa0e06f90c367a55abdf055fba82
kismatthapamagar1 <000kismatthapa@gmail.com>
2026-08-28 12:47:05 +0545

    conversion of datatypes

---
+++ datatypeconversion.js
@@ -0,0 +1,22 @@
+let score ="55abc"
+console.log(typeof score) //console.log(typeof(score)) are similar
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber); //NAN data
+ 
+
+// "33"-33
+// "55abc"-NAN
+//true->1 false-0
+
+
+let isloggedIn =1
+
+let booleanIsLoggedIn =Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+//1-boolean true ""-> false
+
+let someNumbers=44;
+let stringNumbers=String(somenumbers)
+console.log("stringNumbers")
