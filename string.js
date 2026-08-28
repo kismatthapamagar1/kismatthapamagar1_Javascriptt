27731650ea0da33cac26c145d04ef52c41605fa1
kismatthapamagar1 <000kismatthapa@gmail.com>
2026-08-28 13:59:51 +0545

    work on the string

---
+++ string.js
@@ -0,0 +1,13 @@
+const name="kismat"
+const repoCount=4
+//console.log(name + repocount );
+
+console.log(`Hello my name is ${name}and my repo count ${repoCount}`)//backtiks``
+
+const gameName =new String('landra')
+console.log(gameName[0]);
+console.log(gameName[0].__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase);
+console.log(gameName.charAt(3));
+
