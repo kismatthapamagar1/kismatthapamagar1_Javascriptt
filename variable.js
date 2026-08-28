53615952b77eef87283f82c566a19445f114d13b
kismatthapamagar1 <000kismatthapa@gmail.com>
2026-08-28 12:04:57 +0545

    fist session

---
+++ variable.js
@@ -0,0 +1,20 @@
+const accountId =144553
+let accountEmail ="000kismatthapa@gmail.com"
+var accountPassword ="12345"
+accountCity ="Pyuthan" //a variable can also be declared as this but this is not to be preferred
+let accountState; //in javascript semicolon lauda ni hunxa nalauda ni hunxa kai farak pardaina
+
+// accountId = 2 not allowed...
+accountEmail ="kismatthapa2625"
+accountPassword="kismat"
+accountCity="bijuwar"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/*
+Prefer not to use the var because of the issue in the block scope and functional scope
+
+
+*/
