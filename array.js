4e8b1f1a7a2eec7041bafca63476a8fd7b1e22b5
kismatthapamagar1 <000kismatthapa@gmail.com>
2026-08-28 17:38:58 +0545

    array basics

---
+++ array.js
@@ -0,0 +1,25 @@
+// Array
+const myArray =[1,2,3,4,5,6]
+const myHeros=["superman","spiderman"]
+const myArr2=new Array(1,2,3,4,5)
+
+
+console.log(myArray[0])
+//Array Methods
+console.log(myArray)
+myArray.unshift(9)
+myArray.shift()
+console.log(myArray.includes(9))
+console.log(myArray.index(9))
+console.log(myArray.index(3))
+
+console.log(myArray)
+
+
+//slice,splice
+console.log("S",myArray)
+const myn1=myArray.slice(1,3)
+
+
+
+
