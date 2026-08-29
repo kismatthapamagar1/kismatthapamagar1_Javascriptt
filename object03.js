6d1db71076dab05b4945d09c980a1e97e1e96666
kismatthapamagar1 <000kismatthapa@gmail.com>
2026-08-29 08:45:15 +0545

    destructure in object

---
+++ object03.js
@@ -0,0 +1,11 @@
+const course ={
+    courseName:"javascript",
+    coursePrice:"999",
+    courseInstructor:"Kismat Thapa Magar"
+}
+// course.courseInstructor
+
+
+const {courseInstructor: instructor}=course
+console.log(courseInstructor)
+console.log(instructor)
