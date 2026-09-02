6acc7e24adf58821b59a2bafc7631fbb7bc2d62b
kismatthapamagar1 <000kismatthapa@gmail.com>
2026-09-02 07:57:57 +0545

    introduction to js function

---
+++ jsfunction.js
@@ -0,0 +1,32 @@
+// console.log("K");
+// console.log("i");
+// console.log("s");
+// console.log("m");
+// console.log("a");
+// console.log("t");
+
+
+
+
+
+function sayMyName()
+{
+console.log("K");
+console.log("i");
+console.log("s");
+console.log("m");
+console.log("a");
+console.log("t");
+
+
+
+}
+
+
+// sayMyName()
+
+function addTwoNumbers(number1,number2)
+    {
+        console.log(number1+number2);
+    }
+addTwoNumbers(4,5);    
