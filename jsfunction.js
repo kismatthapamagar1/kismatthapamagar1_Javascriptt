262dcd0fb3581955960fb1ee16942c1a50d975b3
kismatthapamagar1 <000kismatthapa@gmail.com>
2026-09-04 21:10:07 +0545

    more about functions

--- jsfunction.js
+++ jsfunction.js
@@ -25,8 +25,34 @@ console.log("t");
 
 // sayMyName()
 
-function addTwoNumbers(number1,number2)
-    {
-        console.log(number1+number2);
+// function addTwoNumbers(number1,number2)
+//     {
+//         console.log(number1+number2);
+//     }
+// addTwoNumbers(4,5);   
+function addTwoNumbers(number1,number2){
+    //let result = number1 + number2
+    // console.log("Kismat")//this line will be displayed
+
+    // return result
+    return number1+number2
+    console.log("Kismat")//this line will not be displayed because after return statement the function is terminated
+}
+const result=addTwoNumbers(4,5)
+// console.log("Result:",result)
+
+function loginUserMessage(username="Raja")
+{
+    // if(username===undefined) is same as below
+    if(!username){
+        console.log("Please Enter a Username")
+        return
+
     }
-addTwoNumbers(4,5);    
+    return `${username} just logged in `
+}
+
+// console.log(loginUserMessage("kismat"))
+console.log(loginUserMessage(""))
+console.log(loginUserMessage())
+
